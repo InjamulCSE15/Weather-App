509f0aa04c9abb8ad270dfec9a270923c582ed7a
unknown <yeasin.mohammod018@gmail.com>
2021-09-09 13:16:22 +0600

    all functionality are ok

--- myScript.js
+++ myScript.js
@@ -27,6 +27,8 @@ let weather = {
 
             document.querySelector(".weather").classList.remove("loading");
 
+            document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')";
+
         },
 
         search: function() {
