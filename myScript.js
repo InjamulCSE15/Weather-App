29d05dd012f9e5555e9c84f582109413550eddf7
unknown <yeasin.mohammod018@gmail.com>
2021-09-09 12:48:04 +0600

    js file editing ...

--- myScript.js
+++ myScript.js
@@ -1,10 +1,29 @@
 
-
 let weather = {
         "apiKey": "5dbfd6784473477e9cb8fcc026ec2fcb",
-        fetchWeather: function () {
-            fetch("https://api.openweathermap.org/data/2.5/weather?q=Chittagong&units=metric&appid=5dbfd6784473477e9cb8fcc026ec2fcb")
+        
+        fetchWeather: function (city) {
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
+                )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+        },
+
+        displayWeather: function (data) {
+            const {name} = data;
+            const {icon, description} = data.weather[0];
+            const {temp, humidity} = data.main;
+            const {speed} = data.wind;
+            
+            //console.log(name, icon, description, temp, humidity, speed);
+
+            document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
+            document.querySelector(".description").innerText = description;
+            document.querySelector(".temperature").innerText = temp + "°C";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+
         }
-}
+};
